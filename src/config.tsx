5a84ad463a7f3cd58a30b1a6f649a83604436e43
Jackson Kaindume <jackson@snowfork.com>
2020-10-22 09:21:39 +0200

    "Initial bridge code"
    This transfers the existing Bridge App code making it a
    standalone application.

---
+++ src/config.tsx
@@ -0,0 +1,11 @@
+// Copyright 2017-2020 @polkadot/apps-routing authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// Bridge applications
+export const APP_ETH_CONTRACT_ADDRESS = '0x19a04AaE0c7F929d8BC0B7f69D106310f168B500';
+export const APP_ERC20_CONTRACT_ADDRESS = '0xF1becfDcA540605451553b4d5f80ACF17C7A490A';
+
+
+// Fetch chain data interval
+export const REFRESH_INTERVAL_MILLISECONDS = 10000;
