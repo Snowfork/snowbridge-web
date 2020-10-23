97c2f4649809247b8d4337de970146eb55f83c64
Jackson Kaindume <jackson@snowfork.com>
2020-10-23 14:56:15 +0200

    remove className Prop

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// global app props
-import { AppProps as Props } from '@polkadot/react-components/types';
-
 // external imports
 import React from 'react';
 import Web3 from 'web3';
@@ -17,7 +14,7 @@ type MyWindow = (typeof window) & {
   web3: Web3;
 }
 
-function BridgeApp ({ className }: Props): React.ReactElement<Props> {
+function BridgeApp () {
 
   const ethEnabled = () => {
     let locWindow = (window as MyWindow);
@@ -35,7 +32,7 @@ function BridgeApp ({ className }: Props): React.ReactElement<Props> {
   }
 
   return (
-      <main className={className}>
+      <main>
         <Bridge web3={(window as MyWindow).web3}/>
       </main>
   );
