7e011b1f5a618840243663a30d0062af94acf209
Jackson Kaindume <jackson@snowfork.com>
2020-11-09 15:59:46 +0200

    intergrade Polkadot API

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@
 // external imports
 import React, {useState, useEffect} from 'react';
 import Web3 from 'web3';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 // local imports and components
 import Bridge from './Bridge';
+import {POLKADOT_API_PROVIDER} from './config'
 
 type MyWindow = (typeof window) & {
   ethereum: any;
@@ -16,6 +18,7 @@ type MyWindow = (typeof window) & {
 
 function BridgeApp () {
   const [web3Enabled, enableWeb3] = useState(false);
+  const [polkadotEnabled, enablePolkadotApi] = useState<null | ApiPromise | 'loading'>('loading');
 
   // Connect to Web3
   useEffect ( () => {
@@ -29,14 +32,90 @@ function BridgeApp () {
 
   }, []);
 
+  // Connect to Polkadotjs
+  useEffect (() => {
+    let exe = async () => {
+      const wsProvider = new WsProvider(POLKADOT_API_PROVIDER);
+
+      try {
+        const api = await ApiPromise.create({
+          provider: wsProvider,
+          types: {
+
+            Address: 'AccountId',
+
+            LookupSource: 'AccountId',
+
+            AppId: '[u8; 20]',
+
+            Message: {
+
+                payload: 'Vec<u8>',
+
+                verification: 'VerificationInput'
+
+            },
+
+            VerificationInput: {
+
+                _enum: {
+
+                    Basic: 'VerificationBasic',
+
+                    None: null
+
+                }
+
+            },
+
+            VerificationBasic: {
+
+                blockNumber: 'u64',
+
+                eventIndex: 'u32'
+
+            },
+
+            TokenId: 'H160',
+
+            BridgedAssetId: 'H160',
+
+            AssetAccountData: {
+
+                free: 'U256'
+
+            }
+
+          }
+        });
+
+        enablePolkadotApi(api);
+
+      } catch(err) {
+        console.log(err)
+        enablePolkadotApi(null);
+      }
+    }
+
+    exe();
+  }, []);
+
   if(!web3Enabled) {
     return <p style={{textAlign: 'center'}}>Please install MetaMask to use this application!</p>
   }
 
+  if(!polkadotEnabled) {
+    return <p style={{textAlign: 'center'}}>
+      Something went wrong while connecting the Polkadotjs API.
+    </p>
+  } else if(polkadotEnabled === 'loading') {
+    return <p style={{textAlign: 'center'}}>Connecting Polkadotjs API...</p>
+  }
+
   return (
-      <main>
-        <Bridge web3={(window as MyWindow).web3}/>
-      </main>
+    <main>
+      <Bridge web3={(window as MyWindow).web3}/>
+    </main>
   );
 }
 
