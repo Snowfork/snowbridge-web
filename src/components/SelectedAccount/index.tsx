a70218b0e3eca3f7463cc1a99c889ac2ad1da672
Jackson Kaindume <jackson@snowfork.com>
2020-11-26 16:22:21 +0200

    add SelectedAccount component

---
+++ src/components/SelectedAccount/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Net from '../../net/';
+import Identicon from '@polkadot/react-identicon';
+
+type Props = {
+  address: string;
+};
+
+export default function SelectAccount({ address }: Props) {
+  const size = 32;
+  const theme = 'polkadot';
+
+  return (
+    <div>
+      <Identicon value={address} size={size} theme={theme} />
+      <span style={{ marginLeft: 15 }}>{address}</span>
+    </div>
+  );
+}
