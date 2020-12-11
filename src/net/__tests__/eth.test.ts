66970e91ac554f16a9ea4397e2d403d0fb9d4592
Jackson Kaindume <jackson@snowfork.com>
2020-12-11 14:25:37 +0200

    add initial tests for the eth API

---
+++ src/net/__tests__/eth.test.ts
@@ -0,0 +1,29 @@
+import Eth from '../eth.ts';
+let eth;
+
+beforeAll(async () => {
+  // Initialize Eth API
+  eth = new Eth(await Eth.connect(), {});
+});
+
+afterAll(() => {
+  // TODO: Disconnect Eth API?
+});
+
+test('Eth Api connection', () => {
+  expect(typeof eth.conn).toBe('object');
+});
+
+test('Get default Ethereum address', async () => {
+  let address = await eth.get_address();
+
+  expect(typeof address).toBe('string');
+  expect(address.length).toBe(42);
+});
+
+test('Get balance of the default Address', async () => {
+  let balance = await eth.get_balance();
+
+  expect(typeof balance).toBe('string');
+  expect(balance.length).toBe(21);
+});
